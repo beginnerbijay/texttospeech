b1bf97196e94232b8d5ac45a40ee6ed9cb8a8f6e
D Bijayshakti Patro <dbijaypatro@gmail.com>
2023-01-02 21:06:55 +0530

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+document.querySelector("#btn").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+  });
